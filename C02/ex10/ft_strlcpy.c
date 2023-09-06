bd0c3492e7d7043ed94dcf2a0ab781dd3257ced3
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-06 18:34:25 +0300

    third project in piscine 42

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zyasa <zyasa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 17:17:02 by zyasa             #+#    #+#             */
+/*   Updated: 2022/11/15 12:52:07 by zyasa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	len = 0;
+	while (src[len])
+		len++;
+	if (size > 0)
+	{
+		while (src[i] && (i < size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (len);
+}
---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zyasa <zyasa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 17:17:02 by zyasa             #+#    #+#             */
+/*   Updated: 2022/11/15 12:52:07 by zyasa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	hex_low(int n)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	write(1, &hex[n / 16], 1);
+	write(1, &hex[n % 16], 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 0 && str[i] < 32)
+		{
+			write(1, "\\", 1);
+			hex_low(str[i]);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
