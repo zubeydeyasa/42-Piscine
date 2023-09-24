c13148948a2b97744dfc19aee7fc11171f80c30b
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-24 23:01:17 +0300

    sixth project in piscine

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zyasa <zyasa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 17:08:54 by zyasa             #+#    #+#             */
+/*   Updated: 2022/03/08 17:18:45 by zyasa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i < nb)
+	{
+		if (!(nb % i))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (!(ft_is_prime(nb)))
+		return (ft_find_next_prime(++nb));
+	return (nb);
+}
