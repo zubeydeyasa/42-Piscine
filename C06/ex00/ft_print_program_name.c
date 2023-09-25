cc73d7037d2f0a3d378520e51e4dab6878f7cd92
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-25 12:36:23 +0300

    seventh project in piscine

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zyasa <zyasa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 17:08:54 by zyasa             #+#    #+#             */
+/*   Updated: 2022/03/08 17:18:45 by zyasa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+		ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zyasa <zyasa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 17:08:54 by zyasa             #+#    #+#             */
+/*   Updated: 2022/03/08 17:18:45 by zyasa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}
